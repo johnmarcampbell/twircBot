7bdf0ba2ffa74d5a768274573171b11441179713
John <johnmarcampbell@gmail.com>
2016-03-27 20:21:31 -0400

    Add processLine() function

--- TwircBot.py
+++ TwircBot.py
@@ -11,6 +11,7 @@ class TwircBot(object):
 
     def __init__(self, config_file_name):
         """Parse the configuration file to retrieve the config parameters """
+        self.irc = socket.socket()
         self.host='irc.twitch.tv'
         self.port=6667
         config_file = open(config_file_name,"r")
@@ -28,6 +29,16 @@ class TwircBot(object):
 
         config_file.close()
 
+    def processLine(self, line):
+        words = line.split()
+        if words[0] == 'PING': 
+            self.irc.send(bytes('PONG :tmi.twitch.tv\r\n', 'utf-8'))
+            print('I have received a ping!!')
+            irc.send(bytes('PRIVMSG #' + self.nick + ' :I received a PING!!\r\n', 'utf-8'));
+        for word in words:
+            if word == bytes('smart','utf-8'):
+                self.irc.send(bytes('PRIVMSG #' + self.nick + ' :You are smart\r\n', 'utf-8'));
+
 
     def connect(self):
         """Connect to twitch chat"""
@@ -36,24 +47,24 @@ class TwircBot(object):
         oauth_string = bytes('PASS oauth:' + self.oauth + ' \r\n', 'utf-8')
         cap_req_string = bytes('CAP REQ :twitch.tv/membership \r\n', 'utf-8')
 
-        irc = socket.socket()
-        irc.connect((self.host, self.port))
-        irc.send(user_string) 
-        irc.send(oauth_string) 
-        irc.send(nick_string) 
-        irc.send(cap_req_string) 
+        self.irc.connect((self.host, self.port))
+        self.irc.send(user_string) 
+        self.irc.send(oauth_string) 
+        self.irc.send(nick_string) 
+        self.irc.send(cap_req_string) 
 
         for channels in self.channel_list:
             channel_string = bytes('JOIN #' + channels + ' \r\n', 'utf-8')
-            irc.send(channel_string) 
+            self.irc.send(channel_string) 
 
         while True: 
-            data = irc.recv(4096)
+            data = self.irc.recv(4096)
             if data:
                 print(data.decode('utf-8'))
                 log_file = open(self.log_file_name,"a")
                 log_file.write(data.decode('utf-8'))
                 log_file.close()
+                self.processLine(data)
 
 
     def print_config(self):
@@ -76,3 +87,4 @@ class TwircBot(object):
         config_string += "\n***** TwircBot config *****\n"
 
         print(config_string)
+

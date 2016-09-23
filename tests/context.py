6b9ade84e7e1c0a72b9c0f2e333b174e46f227b3
John Campbell <johnmarcampbell@gmail.com>
2016-09-23 12:45:51 -0400

    Add files to help with imports for testing

---
+++ tests/context.py
@@ -0,0 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from twircbot.twircbot import *
+from twircbot.botmodule import *
+from twircbot.logger import *
+from twircbot.twitchtools import *
